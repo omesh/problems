9fe0a16d829a07a2eaf32f8071b75bef6c35c06c
Omesh Pandita <omeshp@microsoft.com>
2018-09-24 20:05:20 +0530

    update

---
+++ edit_distance.py
@@ -0,0 +1,11 @@
+# given two strings s1, s2 finding the minimum number of insert/delete/replace operations to arrive at same string.
+
+def edit_distance(s1, s2):
+    return 0
+
+
+print (edit_distance("abc", "abc"))
+print (edit_distance("", "abc"))
+print (edit_distance("", ""))
+print (edit_distance("ab", "abc"))
+
--- fill_glasses.py
+++ fill_glasses.py
@@ -8,7 +8,6 @@ def fill_glasses(height_of_stack, volume_of_glass, amount_of_liquid):
     if height_of_stack > 0:
         glasses[0] = amount_of_liquid * 1.0
         for current_level in range(1, height_of_stack):
-            num_glasses_current_level = current_level
             startIndex = get_glasses_for_level(current_level - 1)
             endIndex = get_glasses_for_level(current_level)
             for current_glass in range(startIndex, endIndex):
@@ -19,7 +18,6 @@ def fill_glasses(height_of_stack, volume_of_glass, amount_of_liquid):
                     glasses[current_glass] = volume_of_glass
             
     return glasses
-    
 
 print (fill_glasses(0, 0, 0.0))
 print (fill_glasses(1, 1, 1.0))
